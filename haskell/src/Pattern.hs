544f518fd8f3b4f5b0bcab702894f05948f897fc
Curt J. Sampson <cjs@cynic.net>
2021-10-26 23:38:55 -0500

    Pattern: Start a very ugly parser (currently non-monadic)

--- haskell/src/Pattern.hs
+++ haskell/src/Pattern.hs
@@ -2,6 +2,8 @@
 
 module Pattern where
 
+import Data.Char (isUpper, isLower)
+
 data Pat
     = EVar   String
     | SVar   String
@@ -11,3 +13,30 @@ data Pat
     | Exists String  Pat    | Forall String  Pat
     | Mu     String  Pat    | Nu     String  Pat
     deriving (Show, Eq)
+
+parsePat ∷ String → Pat
+parsePat "⊥"                 = Bot
+parsePat "⊤"                 = Top
+parsePat (x:xs) | isLower(x) = EVar (x:xs)
+parsePat (x:xs) | isUpper(x) = SVar (x:xs)
+parsePat _ = error "Bad input"
+
+----------------------------------------------------------------------
+-- Parser
+
+data PState = PS
+    { input ∷ String
+    }
+    deriving (Show, Eq)
+
+type Parser x = PState → Either PState (PState, x)
+
+anychar ∷ Parser Char
+anychar ps@(PS "") = Left ps
+anychar (PS (firstchar:rest) ) = Right (PS rest, firstchar)
+
+char ∷ Char → Parser ()
+char _ ps@(PS "") = Left ps
+char c ps@(PS (c':rest))
+    | c == c'   = Right (PS rest, ())
+    | otherwise = Left  ps
--- haskell/src/PatternTest.hs
+++ haskell/src/PatternTest.hs
@@ -21,3 +21,21 @@ test_Pat_syntax_example = assertEqual xs xs
         z  = App "z" []
         f  = App  "f"
         df = DApp "f"
+
+-- map     :: (a -> b) -> [a] -> [b]
+-- unzip   :: [(a, b)] -> ([a], [b])
+-- uncurry :: (a -> b -> c) -> (a, b) -> c
+xxx_test_Pat_parse = uncurry assertEqual $ unzip
+        $ map (\(input, expected) -> (expected, parsePat input))
+        [ ("⊥",         Bot)
+        , ("⊤",         Top)
+        , ("x",         EVar "x")
+        , ("X",         SVar "X")
+        , ("x ",        EVar "x")
+        ]
+
+test_anychar_success = assertEqual (anychar (PS "foo")) (Right (PS "oo", 'f'))
+test_anychar_failure = assertEqual (anychar (PS "")) (Left (PS ""))
+
+test_char_success   = assertEqual (char 'c' (PS "cd")) (Right (PS "d", ()))
+test_char_failure   = assertEqual (char 'd' (PS "cd")) (Left  (PS "cd"))
