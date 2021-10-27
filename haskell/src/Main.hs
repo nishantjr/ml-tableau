da6950621a20412a49d97d729e2996a2e7ad9a71
Curt J. Sampson <cjs@cynic.net>
2021-10-26 21:06:02 -0500

    src/TestMain: New top-level Main module running all tests
    The old src/Main.hs now reverts to printing a greeting again.

--- haskell/src/Main.hs
+++ haskell/src/Main.hs
@@ -1,7 +1,4 @@
 module Main where
 
-import SampleTest (htf_SampleTest_thisModulesTests)
-import Test.Framework (htfMain)
-
 main :: IO ()
-main = htfMain htf_SampleTest_thisModulesTests
+main = putStrLn "Hello, world."
