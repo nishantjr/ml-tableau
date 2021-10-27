244b037a733d2a9e120a5e03aedb45bf89af8f8f
Nishant Rodrigues <nishantjr@gmail.com>
2021-10-26 22:29:50 -0500

    Pattern: Complete syntax for patterns

--- haskell/src/Pattern.hs
+++ haskell/src/Pattern.hs
@@ -3,7 +3,11 @@
 module Pattern where
 
 data Pat
-    = EVar String | SVar String
-    | Pat :∧ Pat | Pat :∨ Pat
-    | App String [Pat]
+    = EVar   String
+    | SVar   String
+    | Top                   | Bot
+    | Pat :∧ Pat            | Pat :∨ Pat
+    | App    String [Pat]   | DApp   String [Pat]
+    | Exists String  Pat    | Forall String  Pat
+    | Mu     String  Pat    | Nu     String  Pat
     deriving (Show, Eq)
--- haskell/src/PatternTest.hs
+++ haskell/src/PatternTest.hs
@@ -6,7 +6,18 @@ import Test.Framework
 
 test_Pat_syntax_example = assertEqual xs xs
     where
-        xs = [ ex, sx, ex :∧ sx, ex :∨ sx, f [sx, ex] ]
-        ex = EVar "x"
-        sx = SVar "X"
-        f  = App "f"
+        xs = [ x, s
+             , x :∧ s, x :∨ s
+             , f [s, x], df []
+             , Top, Bot
+             , Exists "x" $ f [x :∧ s]
+             , Forall "x" $ f [x :∧ s]
+             , Mu "X" $ z :∨ f [s]
+             , Nu "X" $ z :∧ f [s]
+
+             ]
+        x  = EVar "x"
+        s  = SVar "X"
+        z  = App "z" []
+        f  = App  "f"
+        df = DApp "f"
