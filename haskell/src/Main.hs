6b65fc7d343d315bc642fa377e9c29216e1c3f80
Curt J. Sampson <cjs@cynic.net>
2021-11-14 20:11:00 -0600

    haskell/ Main: Discover Data.Composition.(.:) :-)
    This is how we can compose functions that take more than one argument.

--- haskell/src/Main.hs
+++ haskell/src/Main.hs
@@ -2,7 +2,7 @@
 module Main where
 
 import Pattern
-import Data.Map (mapWithKey)
+import Data.Composition
 import Data.Key (mapWithKeyM_)
 
 main :: IO ()
@@ -18,8 +18,5 @@ main = do
             return (sig, stmts)
         printout ∷ (Signature, [Statement]) → IO ()
         printout (sig, stmts) = do
-            print sig
-            -- XXX Trying to print the signature in nicer form....
-            -- (XXX Remember to remove unneeded imports after this is working.)
-            -- mapWithKeyM_ (print . (,)) sig
+            mapWithKeyM_ (print .: (,)) sig
             mapM_ print stmts
